d29e82272221782768303fe69a0b240b97f9a53a
damarteplok <damarhuda@live.com>
2018-04-02 05:31:37 +0000

    4 commit

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,15 +53,14 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-        
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'ec2-54-235-109-37.compute-1.amazonaws.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd3phdqjk2crbfq'),
-            'username' => env('DB_USERNAME', 'loznaobgvusydn'),
-            'password' => env('DB_PASSWORD', '72d6d695ca7992de176e6c8d5c63accef1ee56188fdf467aec713e70b4ef5120'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
